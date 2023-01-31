5d1dc506f5db3030bae9531a59452e3dec91ff16
j <Jarrio@users.noreply.github.com>
2023-01-31 19:32:48 +0000

    throw not implemented exception for certaim image loading methods

--- haxe/ui/backend/AssetsImpl.hx
+++ haxe/ui/backend/AssetsImpl.hx
@@ -12,36 +12,17 @@ using StringTools;
 
 class AssetsImpl extends AssetsBase {
 	private override function getImageFromHaxeResource(resourceId:String, callback:String->ImageInfo->Void):Void {
-		if (Resource.listNames().indexOf(resourceId) == -1) {
-			callback(resourceId, null);
-		} else {
-			var bytes = Resource.getBytes(resourceId);
-			imageFromBytes(bytes, callback.bind(resourceId));
-		}
+		throw haxe.exceptions.NotImplementException;
+		// if (Resource.listNames().indexOf(resourceId) == -1) {
+		// 	callback(resourceId, null);
+		// } else {
+		// 	var bytes = Resource.getBytes(resourceId);
+		// 	imageFromBytes(bytes, callback.bind(resourceId));
+		// }
 	}
 
 	private override function getImageInternal(resourceId:String, callback:ImageInfo->Void):Void {
-		this.imageFromFile(resourceId, callback);
-		// var graphic:Texture = null;
-		// //var frame:FlxFrame = null;
-		// trace(Assets.all);
-
-		// if (Assets.allByName.exists(resourceId)) {
-		// 		graphic = Assets.allByName.get(resourceId);
-		// 		frame = FlxImageFrame.fromGraphic(graphic).frame;
-		// }
-
-		// if (frame != null) {
-		// 		frame.parent.persist = true;
-		// 		frame.parent.destroyOnNoUse = false;
-		// 		callback({
-		// 				data : frame,
-		// 				width : Std.int(frame.sourceSize.x),
-		// 				height : Std.int(frame.sourceSize.y)
-		// 		});
-		// } else {
-		// 		callback(null);
-		// }
+		throw haxe.exceptions.NotImplementException;
 	}
 
 	var assets:Assets;
@@ -76,45 +57,9 @@ class AssetsImpl extends AssetsBase {
 			}
 		});
 		assets.load();
-		// assets.load();
-		// 	kha.Assets.loadImageFromPath(filename, false, function( img ) {
-		// 			callback({
-		// 					data: img,
-		// 					width: img.width,
-		// 					height: img.height,
-		// 			});
-		// 	}, function( err ) {
-		// 			#if debug trace(err); #end
-		// 			callback(null);
-		// 	});
 	}
 
 	public override function imageFromBytes(bytes:Bytes, callback:ImageInfo->Void) {
-		callback(null);
-		// #if kha_krom
-		// var path = Krom.getFilesLocation()+"file."+extensionFromMagicBytes(bytes);
-		// Krom.fileSaveBytes(path,bytes.getData());
-		// kha.Assets.loadImageFromPath(path,false, function(image) {
-		//     var imageInfo:ImageInfo = {
-		//         width: image.realWidth,
-		//         height: image.realHeight,
-		//         data: image
-		//     }
-		//     callback(imageInfo);
-		// });
-		// #else
-
-		// Image.fromEncodedBytes(bytes, extensionFromMagicBytes(bytes), function(image) {
-		//     var imageInfo:ImageInfo = {
-		//         width: image.realWidth,
-		//         height: image.realHeight,
-		//         data: image
-		//     }
-		//     callback(imageInfo);
-		// }, function(error) {
-		//     trace("Problem loading image: " + error);
-		//     callback(null);
-		// });
-		// #end
+		throw haxe.exceptions.NotImplementException;
 	}
 }
