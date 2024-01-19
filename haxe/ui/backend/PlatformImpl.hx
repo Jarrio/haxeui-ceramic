ae64104287d72f947e687a0e8026db024b34e8d5
Jarrio <Jarrio@users.noreply.github.com>
2024-01-19 02:03:54 +0000

    map ceramic keycodes

--- haxe/ui/backend/PlatformImpl.hx
+++ haxe/ui/backend/PlatformImpl.hx
@@ -1,3 +1,18 @@
 package haxe.ui.backend;
 
-class PlatformImpl extends PlatformBase {}
+import ceramic.KeyCode;
+
+class PlatformImpl extends PlatformBase {
+	override function getKeyCode(keyId:String):Int {
+		return switch(keyId) {
+			case 'enter': KeyCode.ENTER;
+			case 'space': KeyCode.SPACE;
+			case 'tab': KeyCode.TAB;
+			case 'up': KeyCode.UP;
+			case 'down': KeyCode.DOWN;
+			case 'left': KeyCode.LEFT;
+			case 'right': KeyCode.RIGHT;
+			case _: keyId.charCodeAt(0);
+		}
+	}
+}
