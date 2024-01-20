678f934c9d2f1b2e1fadc8a7604e9fca76750ece
Jarrio <Jarrio@users.noreply.github.com>
2024-01-20 16:54:11 +0000

    add file save and open dialog for js target

--- haxe/ui/backend/OpenFileDialogImpl.hx
+++ haxe/ui/backend/OpenFileDialogImpl.hx
@@ -1,22 +1,43 @@
 package haxe.ui.backend;
 
-import electron.main.Dialog;
+//import ceramic.Dialogs;
 import haxe.ui.containers.dialogs.Dialogs.SelectedFileInfo;
-import ceramic.Dialogs;
 
 using StringTools;
 
 class OpenFileDialogImpl extends OpenFileDialogBase {
-    public override function show() {
-			trace('not implemented');
-			// Dialogs.openFile('Open file', [], (file) -> {
-			// 	if (file != null) {
-			// 		dialogConfirmed([{
-			// 			fullPath: file
-			// 		}]);
-			// 	} else {
-			// 		dialogCancelled();
-			// 	}
-			// });
-    }
+	#if js
+	private var _fileSelector:haxe.ui.util.html5.FileSelector = new haxe.ui.util.html5.FileSelector();
+
+	public override function show() {
+		var readMode = haxe.ui.util.html5.FileSelector.ReadMode.None;
+		if (options.readContents == true) {
+			if (options.readAsBinary == false) {
+				readMode = haxe.ui.util.html5.FileSelector.ReadMode.Text;
+			} else {
+				readMode = haxe.ui.util.html5.FileSelector.ReadMode.Binary;
+			}
+		}
+		_fileSelector.selectFile(onFileSelected, readMode, options.multiple, options.extensions);
+	}
+
+	private function onFileSelected(cancelled:Bool, files:Array<SelectedFileInfo>) {
+		if (cancelled == false) {
+			dialogConfirmed(files);
+		} else {
+			dialogCancelled();
+		}
+	}
+	#else
+	public override function show() {
+		trace('not implemented on this target yet');
+		// Dialogs.saveFile('Open file', [], (file) -> {
+		// 	if (file != null) {
+		// 		dialogConfirmed();
+		// 	} else {
+		// 		dialogCancelled();
+		// 	}
+		// });
+	}
+	#end
 }
--- haxe/ui/backend/SaveFileDialogImpl.hx
+++ haxe/ui/backend/SaveFileDialogImpl.hx
@@ -1,10 +1,35 @@
 package haxe.ui.backend;
 
-import ceramic.Dialogs;
+//import ceramic.Dialogs;
 
 class SaveFileDialogImpl extends SaveFileDialogBase {
+
+
+	#if js
+	private var _fileSaver:haxe.ui.util.html5.FileSaver = new haxe.ui.util.html5.FileSaver();
+
 	public override function show() {
-		trace('not implemented');
+		if (fileInfo == null || (fileInfo.text == null && fileInfo.bytes == null)) {
+			throw "Nothing to write";
+		}
+
+		if (fileInfo.text != null) {
+			_fileSaver.saveText(fileInfo.name, fileInfo.text, onSaveResult);
+		} else if (fileInfo.bytes != null) {
+			_fileSaver.saveBinary(fileInfo.name, fileInfo.bytes, onSaveResult);
+		}
+	}
+
+	private function onSaveResult(r:Bool) {
+		if (r == true) {
+			dialogConfirmed();
+		} else {
+			dialogCancelled();
+		}
+	}
+	#else
+	public override function show() {
+		trace('not implemented on this target yet');2
 		// Dialogs.saveFile('Open file', [], (file) -> {
 		// 	if (file != null) {
 		// 		dialogConfirmed();
@@ -13,4 +38,5 @@ class SaveFileDialogImpl extends SaveFileDialogBase {
 		// 	}
 		// });
 	}
+	#end
 }
