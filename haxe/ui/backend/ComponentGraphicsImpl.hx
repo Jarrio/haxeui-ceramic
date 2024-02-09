df1f8ff7567a21c260a2a5e82b1bb3c7c5351e8b
Jarrio <Jarrio@users.noreply.github.com>
2024-02-09 18:20:09 +0000

    Add some canvas api things

--- haxe/ui/backend/ComponentGraphicsImpl.hx
+++ haxe/ui/backend/ComponentGraphicsImpl.hx
@@ -1,27 +1,142 @@
 package haxe.ui.backend;
+
+import ceramic.App;
+import haxe.ui.util.Color;
+import ceramic.Line;
+import ceramic.Point;
 import haxe.ui.core.Component;
 import ceramic.RenderTexture;
 import ceramic.Quad;
 import haxe.io.Bytes;
 import ceramic.Texture;
 import ceramic.UInt8Array;
+import ceramic.Arc;
 
 class ComponentGraphicsImpl extends ComponentGraphicsBase {
 	var render:RenderTexture;
 	var texture:Texture;
 	var visual:Quad;
 	var hasSize = false;
+
+	var current_position:Point = new Point();
+	
+	var color:ceramic.Color;
+	var thickness:Float;
+	var alpha:Float = 1;
+
+	var size:Bool = false;
+
+	var line = new Line();
+	var quad:Quad = new Quad();
+	var arcs:Arc = new Arc();
+
 	public function new(component:Component) {
 		super(component);
-		render = new RenderTexture(100, 100);
+		//line.color = ceramic.Color.BLACK;
+		//App.app.onBeginDraw(visual, draw);
 	}
 
 	public override function clear() {
-		render.clear(function() {});
+		if (!size) {
+			return super.clear();
+		}
+		//render.clear(() -> {});
+	}
+
+	// function draw() {
+	// 	for (key => command in _drawCommands) {
+	// 		switch (command) {
+	// 			case Clear:
+	// 			case MoveTo(x, y):
+	// 				this.current_position.x = x;
+	// 				this.current_position.y = y;
+	// 			case LineTo(x, y):
+	// 				// var line = new Line();
+				
+	// 				// line.color = this.color;
+	// 				// line.thickness = this.thickness;
+	// 				// line.points = [current_position.x, current_position.y, x, y];
+	// 				// render.stamp(line, () -> {
+	// 				// 	line.destroy();
+	// 				// });
+	// 			case StrokeStyle(color, thickness, alpha):
+	// 				this.color = ceramic.Color.BLACK;
+	// 				this.thickness = thickness;
+	// 				this.alpha = alpha;
+	// 			default:
+	// 		}
+	// 	}
+	// }
+
+	override function strokeStyle(color:Null<Color>, thickness:Null<Float> = 1, alpha:Null<Float> = 1) {
+		if (!size) {
+			return super.strokeStyle(color, thickness, alpha);
+		}
+		this.color = ceramic.Color.BLACK;
+		this.thickness = thickness;
+		this.alpha = alpha;
+	}
+
+	override function moveTo(x:Float, y:Float) {
+		if (!size) {
+			return super.moveTo(x, y);
+		}
+		current_position.x = x;
+		current_position.y = y;
+	}
+
+	override function lineTo(x:Float, y:Float) {
+		if (!size) {
+			return super.lineTo(x, y);
+		}
+
+		var line = new Line();
+		line.color = this.color;
+		line.thickness = this.thickness;
+		line.points = [current_position.x, current_position.y, x, y];
+		render.stamp(line, () -> {
+			line.destroy();
+		});
+	}
+
+	override function rectangle(x:Float, y:Float, width:Float, height:Float) {
+		if (!size) {
+			return super.rectangle(x, y, width, height);
+		}
+		var quad = new Quad();
+		quad.color = this.color;
+		quad.alpha = this.alpha;
+		quad.x = x;
+		quad.y = y;
+		quad.width = width;
+		quad.height = height;
+		render.stamp(quad, () -> {
+			quad.destroy();
+		});
+	}
+
+	override function circle(x:Float, y:Float, radius:Float) {
+		if (!size) {
+			return super.circle(x, y, radius);
+		}
+		var arc = new Arc();
+		arc.borderPosition = MIDDLE;
+		arc.radius = radius;
+		arc.angle = 360;
+		arc.color = this.color;
+		arc.alpha = this.alpha;
+		arc.thickness = 1;
+		arc.x = x;
+		arc.y = y;
+		render.stamp(arc, () -> {
+			arc.destroy();
+		});
 	}
 
 	public override function setPixels(pixels:Bytes) {
-
+		if (!size) {
+			return super.setPixels(pixels);
+		}
 		var w = Std.int(_component.width);
 		var h = Std.int(_component.height);
 
@@ -31,14 +146,35 @@ class ComponentGraphicsImpl extends ComponentGraphicsBase {
 			if (texture.width != _component.width || texture.height != _component.height) {
 				texture = Texture.fromPixels(w, h, UInt8Array.fromBytes(pixels));
 			}
-			texture.submitPixels(UInt8Array.fromBytes(pixels));
+			
+			render.submitPixels(UInt8Array.fromBytes(pixels));
 		}
 
 		if (this.visual == null) {
 			visual = new Quad();
-			visual.texture = texture;
 			visual.size(w, h);
 			_component.visual.add(visual);
+		}
+
+		if (this.visual.texture == null) {
+			visual.texture = texture;
+		}
+	}
+
+	public override function resize(width:Null<Float>, height:Null<Float>) {
+		if (width > 0 && height > 0) {
+			if (!size) {
+				size = true;
+				
+				visual = new Quad();
+				render = new RenderTexture(width, height);
+				
+				render.autoRender = false;
+				visual.texture = render;
+				_component.visual.add(visual);
+				visual.size(width, height);
+				replayDrawCommands();
+			}
 		}
 	}
 }
