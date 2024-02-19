dd93e806aa364c9763ae2ff2be024a872d41cac5
Jarrio <Jarrio@users.noreply.github.com>
2024-02-19 09:09:22 +0000

    force re renders when text input changes

--- haxe/ui/backend/TextInputImpl.hx
+++ haxe/ui/backend/TextInputImpl.hx
@@ -14,14 +14,28 @@ class TextInputImpl extends TextDisplayImpl {
 		field = new EditText(Color.BLUE, Color.BLACK);
 		visual.component('edit_text', field);
 		field.onUpdate(visual, this.onTextChanged);
+		field.onStart(visual, this.onStart);
+		field.onStop(visual, this.onStop);
+	}
+
+	function onStop() {
+		unregisterEvents();
+		Ceramic.forceRender();
+	}
+
+	function onStart() {
+		registerEvents();
+		Ceramic.forceRender();
 	}
 
 	public override function focus() {
 		field.focus();
+		Ceramic.forceRender();
 	}
 
 	public override function blur() {
 		field.stopInput();
+		Ceramic.forceRender();
 	}
 
 	private var _eventsRegistered:Bool = false;
@@ -47,23 +61,27 @@ class TextInputImpl extends TextDisplayImpl {
 		var measureTextRequired:Bool = super.validateStyle();
 
 		field.disabled = parentComponent.disabled;
-		
+		Ceramic.forceRender();
 		return measureTextRequired;
 	}
 
+
 	function onTextChanged(text:String) {
-		if (text == _text) {
-			return;
-		}
 		_text = text;
 		measureText();
-        
+
 		if (_inputData.onChangedCallback != null) {
 			_inputData.onChangedCallback();
 		}
-		
+
 		if (parentComponent != null) {
 			parentComponent.dispatch(new UIEvent(UIEvent.CHANGE));
 		}
+		Ceramic.forceRender();
 	}
+
+    private override function validatePosition() {
+			_left = Math.round(_left);
+			_top = Math.round(_top);
+    }
 }
